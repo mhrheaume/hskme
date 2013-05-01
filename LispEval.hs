77abbf33925ff7d1239d54c4885db3b0b56a5909
Matthew Rheaume <mhrheaume@gmail.com>
2013-05-01 03:19:38 -0400

    More primitive functions.

--- LispEval.hs
+++ LispEval.hs
@@ -15,15 +15,51 @@ primitives =
 	 ("/", numericBinop div),
 	 ("mod", numericBinop mod),
 	 ("quotient", numericBinop quot),
-	 ("remainder", numericBinop rem)]
+	 ("remainder", numericBinop rem),
+	 ("=", numBoolBinop (==)),
+	 ("<", numBoolBinop (<)),
+	 (">", numBoolBinop (>)),
+	 ("/=", numBoolBinop (/=)),
+	 (">=", numBoolBinop (>=)),
+	 ("<=", numBoolBinop (<=)),
+	 ("&&", boolBoolBinop (&&)),
+	 ("||", boolBoolBinop (||)),
+	 ("string=?", strBoolBinop (==)),
+	 ("string<?", strBoolBinop (<)),
+	 ("string>?", strBoolBinop (>)),
+	 ("string<=?", strBoolBinop (<=)),
+	 ("string>=?", strBoolBinop (>=))]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params = mapM unpackNum params >>= return . LispNumber . foldl1 op
 
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args =
+	if length args /= 2
+	then throwError $ NumArgs 2 args
+	else do
+		left <- unpacker $ args !! 0
+		right <- unpacker $ args !! 1
+		return $ LispBool $ left `op` right
+
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+boolBoolBinop = boolBinop unpackBool
+
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (LispNumber n) = return n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (LispString s) = return s
+unpackStr (LispNumber s) = return $ show s
+unpackStr (LispBool s) = return $ show s
+unpackStr other = throwError $ TypeMismatch "string" other
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (LispBool b) = return b
+unpackBool other = throwError $ TypeMismatch "boolean" other
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe
